2fe50d0630a7eac1c206fd7f76924a711bf314c0
Josef Šimánek <josef.simanek@gmail.com>
2014-03-23 01:51:23 +0100

    Init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var rssurl = 'http://www.corsproxy.com/www.rouming.cz/roumingRSS.php';
+
+$(document).ready(function() {
+
+    var $data = new Array();
+    $.get(rssurl, function(data) {
+        var xml = $(data);
+        xml.find("item").each(function() {
+            var title = $(this).find("title").text();
+            var link = 'http://www.rouming.cz/upload/' + title;
+            if(title != 'Rouming') {
+                $data.push({lowsrc: link, fullsrc: link, description: title, category: "roumen"});
+            }
+        });
+
+        $('#gallery').galereya({
+            load: function(next) {
+                next($data);
+            }
+        });
+    });
+});
